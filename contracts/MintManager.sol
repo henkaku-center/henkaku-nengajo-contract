aa44093c2dbca8ee90abe78a451473f20ec31e67
yawn <69970183+yawn-c111@users.noreply.github.com>
2022-11-10 14:33:27 +0900

    feat: add abstracted MintManager

---
+++ contracts/MintManager.sol
@@ -0,0 +1,56 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+
+abstract contract MintManager {
+    bool public mintable;
+    uint256 public immutable open_blockTimestamp;
+    uint256 public immutable close_blockTimestamp;
+
+    mapping(address => bool) public admins;
+
+    constructor(uint256 _open_blockTimestamp, uint256 _close_blockTimestamp) {
+        admins[msg.sender] = true;
+        open_blockTimestamp = _open_blockTimestamp;
+        close_blockTimestamp = _close_blockTimestamp;
+    }
+
+    modifier onlyAdmins() {
+        require(admins[msg.sender], "Admins only");
+        _;
+    }
+
+    function addAdmins(address[] memory _newAdmins) external onlyAdmins {
+        uint256 _newAdminsLength = _newAdmins.length;
+        require(_newAdminsLength > 0, "Need one or more new admins");
+
+        if (_newAdminsLength == 1 && !admins[_newAdmins[0]]) {
+            admins[_newAdmins[0]] = true;
+        } else {
+            for (uint256 i = 0; i < _newAdminsLength; ++i) {
+                if (!admins[_newAdmins[i]]) {
+                    admins[_newAdmins[i]] = true;
+                }
+            }
+        }
+    }
+
+    function switchMintable() external onlyAdmins {
+        mintable = !mintable;
+    }
+
+    function checkRemainingOpenTime() external view returns (uint256) {
+        if (open_blockTimestamp > block.timestamp) {
+            return open_blockTimestamp - block.timestamp;
+        } else {
+            return 0;
+        }
+    }
+
+    function checkRemainingCloseTime() external view returns (uint256) {
+        if (close_blockTimestamp > block.timestamp) {
+            return close_blockTimestamp - block.timestamp;
+        } else {
+            return 0;
+        }
+    }
+}
--- contracts/Nengajo.sol
+++ contracts/Nengajo.sol
@@ -1,74 +1,28 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.9;
+pragma solidity ^0.8.17;
 
 import "@openzeppelin/contracts/token/ERC1155/extensions/ERC1155Supply.sol";
 import "@openzeppelin/contracts/token/ERC1155/extensions/ERC1155URIStorage.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
+import "./MintManager.sol";
 
-contract Nengajo is ERC1155, ERC1155Supply, ERC1155URIStorage {
+contract Nengajo is ERC1155, ERC1155Supply, ERC1155URIStorage, MintManager {
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
 
     string public name;
     string public symbol;
-    bool public mintable;
-    uint256 public immutable open_blockTimestamp;
-    uint256 public immutable close_blockTimestamp;
 
     mapping(uint256 => uint256) private maxSupply;
-    mapping(address => bool) public admins;
 
     constructor(
         string memory _name,
         string memory _symbol,
         uint256 _open_blockTimestamp,
         uint256 _close_blockTimestamp
-    ) ERC1155("") {
+    ) ERC1155("") MintManager(_open_blockTimestamp, _close_blockTimestamp) {
         name = _name;
         symbol = _symbol;
-        admins[msg.sender] = true;
-        open_blockTimestamp = _open_blockTimestamp;
-        close_blockTimestamp = _close_blockTimestamp;
-    }
-
-    modifier onlyAdmins() {
-        require(admins[msg.sender], "Admins only");
-        _;
-    }
-
-    function addAdmins(address[] memory _newAdmins) external onlyAdmins {
-        uint256 _newAdminsLength = _newAdmins.length;
-        require(_newAdminsLength > 0, "Need one or more new admins");
-
-        if (_newAdminsLength == 1 && !admins[_newAdmins[0]]) {
-            admins[_newAdmins[0]] = true;
-        } else {
-            for (uint256 i = 0; i < _newAdminsLength; ++i) {
-                if (!admins[_newAdmins[i]]) {
-                    admins[_newAdmins[i]] = true;
-                }
-            }
-        }
-    }
-
-    function switchMintable() external onlyAdmins {
-        mintable = !mintable;
-    }
-
-    function checkRemainingOpenTime() external view returns (uint256) {
-        if (open_blockTimestamp > block.timestamp) {
-            return open_blockTimestamp - block.timestamp;
-        } else {
-            return 0;
-        }
-    }
-
-    function checkRemainingCloseTime() external view returns (uint256) {
-        if (close_blockTimestamp > block.timestamp) {
-            return close_blockTimestamp - block.timestamp;
-        } else {
-            return 0;
-        }
     }
 
     function registerCreative(uint256 _maxSupply, string memory _metaDataURL)
