6f2e5d85a9de29a7f13146fa751d03b93b1dbfd5
yu23ki14 <yuki_021423@yahoo.co.jp>
2022-12-05 23:36:46 +0900

    fix require message

--- contracts/InteractHenkakuToken.sol
+++ contracts/InteractHenkakuToken.sol
@@ -15,7 +15,7 @@ abstract contract InteractHenakuToken {
     function transferHenkakuV2(uint256 _amount) internal {
         require(checkHenkakuV2Balance(_amount), "Nengajo: Insufficient HenkakuV2 token");
         bool sent = IHenkakuToken(henkakuV2).transferFrom(msg.sender, henkakuPoolWallet, _amount);
-        require(sent, "transferFrom failed");
+        require(sent, "Nengajo: Henkaku transfer failed");
     }
 
     function checkHenkakuV2Balance(uint256 _requiredAmount) internal view returns (bool) {
