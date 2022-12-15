b53e8c6d2c1e8cdb38b0c1107d20db87ad038d7b
yu23ki14 <yuki_021423@yahoo.co.jp>
2022-12-15 13:46:34 +0900

    add ethscan

---
+++ scripts/deploy_testnet.ts
@@ -0,0 +1,38 @@
+import * as dotenv from 'dotenv'
+import { ethers } from 'hardhat'
+
+dotenv.config()
+
+const main = async () => {
+  const localUserAddresses = String(process.env.TESTNET_USERS_ADDRESSES).split(',')
+
+  const HenkakuV2Factory = await ethers.getContractFactory('HenkakuToken')
+  const HenkakuV2Contract = await HenkakuV2Factory.deploy()
+  await HenkakuV2Contract.deployed()
+
+  await HenkakuV2Contract.addWhitelistUsers(localUserAddresses)
+  for (const address of localUserAddresses) {
+    await HenkakuV2Contract.mint(address, 100n ** 13n)
+  }
+
+  const open_blockTimestamp: number = 0
+  const close_blockTimestamp: number = 1000000000
+  const NengajoFactory = await ethers.getContractFactory('Nengajo')
+  const NengajoContract = await NengajoFactory.deploy(
+    'Henkaku Nengajo',
+    'HNJ',
+    open_blockTimestamp,
+    close_blockTimestamp,
+    HenkakuV2Contract.address,
+    localUserAddresses[0]
+  )
+  await NengajoContract.deployed()
+
+  console.log(`HenkakuV2: ${HenkakuV2Contract.address}`)
+  console.log(`Nengajo  : ${NengajoContract.address}`)
+}
+
+main().catch((error) => {
+  console.error(error)
+  process.exitCode = 1
+})
