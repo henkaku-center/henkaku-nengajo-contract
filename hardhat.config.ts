951eddfd841a317ef4db3d513e40530af7544ee5
SHØ <neila@users.noreply.github.com>
2023-05-09 13:59:34 +0900

    improve hardhat config & add README

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -9,29 +9,30 @@ const config: HardhatUserConfig = {
   solidity: '0.8.17',
   networks: {
     polygon: {
-      url: process.env.POLYGON_ALCHEMY_KEY!,
-      accounts: [process.env.MAIN_PRIVATE_KEY!],
+      url:
+        process.env.POLYGON_ALCHEMY_KEY || 'https://polygon-mumbai.g.alchemy.com/v2/123abc123abc123abc123abc123abcde',
+      accounts: process.env.MAIN_PRIVATE_KEY ? [process.env.MAIN_PRIVATE_KEY] : [],
       gasPrice: 110000000000,
     },
     mumbai: {
-      url: process.env.MUMBAI_ALCHEMY_KEY!,
-      accounts: [process.env.TEST_PRIVATE_KEY!],
+      url: process.env.MUMBAI_ALCHEMY_KEY || 'https://polygon.g.alchemy.com/v2/123abc123abc123abc123abc123abcde',
+      accounts: process.env.TEST_PRIVATE_KEY ? [process.env.TEST_PRIVATE_KEY] : [],
     },
     goerli: {
-      url: process.env.GOERLI_ALCHEMY_KEY!,
-      accounts: [process.env.TEST_PRIVATE_KEY!],
+      url: process.env.GOERLI_ALCHEMY_KEY || 'https://polygon-mumbai.g.alchemy.com/v2/123abc123abc123abc123abc123abcde',
+      accounts: process.env.TEST_PRIVATE_KEY ? [process.env.TEST_PRIVATE_KEY] : [],
     },
     local: {
       url: 'http://localhost:8545',
-      accounts: [`${process.env.LOCAL_PRIVATE_KEY}`],
+      accounts: process.env.LOCAL_PRIVATE_KEY ? [process.env.LOCAL_PRIVATE_KEY] : [],
       allowUnlimitedContractSize: true,
     },
   },
   etherscan: {
     apiKey: {
-      polygonMumbai: process.env.POLYGONSCAN_API_KEY!,
-      polygon: process.env.POLYGONSCAN_API_KEY!,
-      goerli: process.env.ETHERSCAN_API_KEY!,
+      polygonMumbai: process.env.POLYGONSCAN_API_KEY ? process.env.POLYGONSCAN_API_KEY : '',
+      polygon: process.env.POLYGONSCAN_API_KEY ? process.env.POLYGONSCAN_API_KEY : '',
+      goerli: process.env.ETHERSCAN_API_KEY ? process.env.ETHERSCAN_API_KEY : '',
     },
   },
 }
