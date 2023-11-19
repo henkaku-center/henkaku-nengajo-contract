311e7fa4c490cd80ba66e46eb055a0d1f7784f17
yawn <69970183+yawn-c111@users.noreply.github.com>
2023-11-19 13:12:39 +0900

    update: scripts for whitelist users and deploy HenkakuToken contract on testnet

--- scripts/community/deploy_testnet.ts
+++ scripts/community/deploy_testnet.ts
@@ -6,14 +6,16 @@ dotenv.config()
 const main = async () => {
   const localUserAddresses = String(process.env.TESTNET_USERS_ADDRESSES).split(',')
 
-  const HenkakuV2Factory = await ethers.getContractFactory('HenkakuToken')
-  const HenkakuV2Contract = await HenkakuV2Factory.deploy()
-  await HenkakuV2Contract.deployed()
+  // const HenkakuV2Factory = await ethers.getContractFactory('HenkakuToken')
+  // const HenkakuV2Contract = await HenkakuV2Factory.deploy()
+  // await HenkakuV2Contract.deployed()
 
-  await HenkakuV2Contract.addWhitelistUsers(localUserAddresses)
-  for (const address of localUserAddresses) {
-    await HenkakuV2Contract.mint(address, ethers.utils.parseEther('1000'))
-  }
+  // await HenkakuV2Contract.addWhitelistUsers(localUserAddresses)
+  // for (const address of localUserAddresses) {
+  //   await HenkakuV2Contract.mint(address, ethers.utils.parseEther('1000'))
+  // }
+
+  const HenkakuV2ContractAddress = ''
 
   const open_blockTimestamp: number = 0
   const close_blockTimestamp: number = 2671458400
@@ -23,12 +25,12 @@ const main = async () => {
     'HNJ',
     open_blockTimestamp,
     close_blockTimestamp,
-    HenkakuV2Contract.address,
+    HenkakuV2ContractAddress,
     localUserAddresses[0]
   )
   await NengajoContract.deployed()
 
-  console.log(`HenkakuV2: ${HenkakuV2Contract.address}`)
+  console.log(`HenkakuV2: ${HenkakuV2ContractAddress}`)
   console.log(`Nengajo  : ${NengajoContract.address}`)
 }
 
