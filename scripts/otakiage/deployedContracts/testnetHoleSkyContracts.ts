28b03f388e50120474ea057359e925773a8880b0
yawn <69970183+yawn-c111@users.noreply.github.com>
2024-12-21 00:10:53 +0900

    feat: add otakiage addresses and verification script

--- scripts/otakiage/deployedContracts/testnetHoleSkyContracts.ts
+++ scripts/otakiage/deployedContracts/testnetHoleSkyContracts.ts
@@ -5,3 +5,7 @@ export const forwarderAddresses = {
 export const omamoriAddresses = {
   '20241220-1': '0xCcC09563b7A69CadfF4b770cC76cb4452858b89C',
 }
+
+export const otakiageAddresses = {
+  '20241220-1': '0x40Ea6a32F6572A1FF7A501bc546fdC936259B289',
+}
