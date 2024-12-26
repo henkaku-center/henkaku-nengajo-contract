b0713b433069fcf90eb1153d36b0bf893ec8a019
yawn <69970183+yawn-c111@users.noreply.github.com>
2024-12-26 16:56:21 +0900

    update: testnet contracts

---
+++ scripts/otakiage/deployedContracts/mainnetPolygonContracts.ts
@@ -0,0 +1,11 @@
+export const forwarderAddresses = {
+  '0': '0x802422C3cc18ED1f765bb6932Ec9A45cc6A1Fa6E',
+}
+
+export const omamoriAddresses = {
+  '0': '0xAC42aA54DfF142b5cEB4196B94cc013bbCf074C5',
+}
+
+export const otakiageAddresses = {
+  '0': '',
+}
